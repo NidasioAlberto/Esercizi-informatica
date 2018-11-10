b1ecda342a85094cb38a19506876d143c4eb2b56
Alberto Nidasio <nidasioalberto@gmail.com>
2018-11-10 10:55:28 +0100

    Fase di analisi del progetto RMI

---
+++ SQL/Creazione dipartimenti.sql
@@ -0,0 +1,14 @@
+use AlbertoNidasio;
+
+/*create table Dipartimenti(codice int, nome varchar(20), sede varchar(20), direttore int,
+    primary key (codice),
+    foreign key (direttore) references Impiegati(matricola));
+
+insert into Dipartimenti (codice, nome, sede, direttore) values(0, "Principale", "Roma", 2);
+insert into Dipartimenti (codice, nome, sede, direttore) values(1, "Sede nord", "Milano", 3);
+insert into Dipartimenti (codice, nome, sede, direttore) values(2, "Napoletana", "Napoli", 5);
+insert into Dipartimenti (codice, nome, sede, direttore) values(3, "Veneziana", "Venezia", 8);
+
+select * from Dipartimenti;*/
+
+delete from Dipartimenti;
